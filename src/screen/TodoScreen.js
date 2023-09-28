d33dc35f63985d8c27d3a87c30b10572eceb5509
ibrataha8 <ibrataha8@gmail.com>
2023-09-28 14:49:19 +0100

    add icon in todo

--- src/screen/TodoScreen.js
+++ src/screen/TodoScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { View, StyleSheet, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
-
+import { IconButton } from 'react-native-paper';
 const listTodos = [{ id: 1, name: "brahim" }, { id: 2, name: "Fati" }, { id: 3, name: "mm" }]
 const TodoScreen = () => {
 
     const renderTodos = ({ item, index }) => {
         return (
-            <View style={{ backgroundColor: "blue", borderRadius: 6,paddingHorizontal:6,paddingVertical:12 }}>
-                <Text style={{ color: "white" }}>{item.name}</Text>
+
+            <View style={{
+                backgroundColor: "orange",
+                borderRadius: 6,
+                paddingHorizontal: 6,
+                paddingVertical: 12,
+                marginVertical: 5,
+                flexDirection: "row",
+                alignItems: "center"
+            }}>
+                <Text style={{ color: "white", fontWeight: "bold", fontSize: 18, flex: 1 }}>{item.name}</Text>
+                <IconButton icon="pencil" />
+                <IconButton icon="trash-can" />
             </View>
         )
     }
@@ -21,7 +32,7 @@ const TodoScreen = () => {
                 paddingVertical: 12
             }}
                 placeholder='Add A Task' />
-            <TouchableOpacity style={{ backgroundColor: "black", borderRadius: 6, paddingVertical: 10, marginTop: 24 }}>
+            <TouchableOpacity style={{ backgroundColor: "black", borderRadius: 6, paddingVertical: 10, marginVertical: 24 }}>
                 <Text style={{ color: "white", fontWeight: "bold", textAlign: "center", fontSize: 20 }}>Add</Text>
             </TouchableOpacity>
             {/* Render todo listt */}
